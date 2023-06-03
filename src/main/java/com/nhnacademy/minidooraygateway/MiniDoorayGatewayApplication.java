b665d28f9973b9013dcbcbe507f3dbbb4b96f313
suebin <suuebin@gmail.com>
2023-06-03 18:39:28 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/minidooraygateway/MiniDoorayGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooraygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoorayGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooraygateway/MiniDoorayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooraygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
