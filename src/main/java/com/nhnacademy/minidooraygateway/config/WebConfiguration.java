8e166f251663b321778b0c60124c6d44db3fa976
suebin <suuebin@gmail.com>
2023-06-11 21:39:50 +0900

    ✨  feat(aouth2): DefaultOAuth2UserService 추가

--- src/main/java/com/nhnacademy/minidooraygateway/config/WebConfiguration.java
+++ src/main/java/com/nhnacademy/minidooraygateway/config/WebConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
-@EnableConfigurationProperties(UrlProperties.class)
+@EnableConfigurationProperties({UrlProperties.class, OAuth2Properties.class})
 public class WebConfiguration {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
