494b9b96c18ee66ef90839892ed77be7281cbf9b
권형택 <naht94@hanmail.net>
2023-06-12 20:10:33 +0900

    🔨Refactor oauth 로그인 오류시 회원가입창으로 넘어가는 처리

---
+++ src/main/java/com/nhnacademy/minidooraygateway/filter/OAuthEmailLoginFailureFilter.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.minidooraygateway.filter;
+
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class OAuthEmailLoginFailureFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (HttpClientErrorException e) {
+            response.sendRedirect("/signUp");
+        }
+    }
+}
