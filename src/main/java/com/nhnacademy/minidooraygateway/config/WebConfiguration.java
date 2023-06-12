43d8a839383638f637e669696a917041fff01407
권형택 <naht94@hanmail.net>
2023-06-12 15:20:21 +0900

    🔬feat(redis) redis session 추가

--- src/main/java/com/nhnacademy/minidooraygateway/config/WebConfiguration.java
+++ src/main/java/com/nhnacademy/minidooraygateway/config/WebConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
-@EnableConfigurationProperties({UrlProperties.class, OAuth2Properties.class, TaskUrlProperties.class})
+@EnableConfigurationProperties({UrlProperties.class, OAuth2Properties.class, TaskUrlProperties.class, RedisConfig.class})
 public class WebConfiguration {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
--- src/main/java/com/nhnacademy/minidooraygateway/service/CustomOAuth2UserService.java
+++ src/main/java/com/nhnacademy/minidooraygateway/service/CustomOAuth2UserService.java
@@ -27,40 +27,39 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-//@Service
-//@RequiredArgsConstructor
-//public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-//    private final OAuth2Properties oAuth2Properties;
-//    private final RestTemplate restTemplate;
-//
-//    @Override
-//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-//        HttpHeaders header = new HttpHeaders();
-//        header.setAccept(List.of(MediaType.valueOf("application/vnd.github+json")));
-//        header.setBasicAuth(oAuth2Properties.getClientId());
-//        header.set("X-GitHub-Api-Version", "2022-11-28");
-//        HttpEntity<List<OAuth2GitEmail>> requestEntity = new HttpEntity<>(header);
-//
-//        ResponseEntity<List<OAuth2GitEmail>> response =
-//                restTemplate.exchange("https://api.github.com/user/emails",
-//                        HttpMethod.GET,
-//                        requestEntity,
-//                        new ParameterizedTypeReference<>() {
-//                        });
-//        List<OAuth2GitEmail> body = response.getBody();
-//        String email = getPrimaryEmail(body);
-//
-//        System.out.println(email);
-//
-//        return new User("hi");
-//    }
-//
-//    private static String getPrimaryEmail(List<OAuth2GitEmail> response) {
-//        return response.stream()
-//                .filter(OAuth2GitEmail::getPrimary)
-//                .findAny()
-//                .orElseThrow()
-//                .getEmail();
-//    }
-//
-//}
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final RestTemplate restTemplate;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        HttpHeaders header = new HttpHeaders();
+        header.setAccept(List.of(MediaType.valueOf("application/vnd.github+json")));
+        header.setBasicAuth(userRequest.getAccessToken().getTokenValue());
+        header.set("X-GitHub-Api-Version", "2022-11-28");
+        HttpEntity<List<OAuth2GitEmail>> requestEntity = new HttpEntity<>(header);
+
+        ResponseEntity<List<OAuth2GitEmail>> response =
+                restTemplate.exchange("https://api.github.com/user/emails",
+                        HttpMethod.GET,
+                        requestEntity,
+                        new ParameterizedTypeReference<>() {
+                        });
+        List<OAuth2GitEmail> body = response.getBody();
+        String email = getPrimaryEmail(body);
+
+        System.out.println(email);
+
+        return new User("hi");
+    }
+
+    private static String getPrimaryEmail(List<OAuth2GitEmail> response) {
+        return response.stream()
+                .filter(OAuth2GitEmail::getPrimary)
+                .findAny()
+                .orElseThrow()
+                .getEmail();
+    }
+
+}
