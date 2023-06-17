86b51b831ef48e8e72d8038cdd3b0e5c8c196b22
suebin <suuebin@gmail.com>
2023-06-17 21:08:35 +0900

    ✨  feat(tag): 태그 관리 기능 구현

--- src/main/java/com/nhnacademy/minidooraygateway/task/domain/Response.java
+++ src/main/java/com/nhnacademy/minidooraygateway/task/domain/Response.java
@@ -2,8 +2,10 @@ package com.nhnacademy.minidooraygateway.task.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Response {
     private String result;
